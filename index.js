70e87b6de8a0a3cda4310410e3151ac9550a4cd6
Thibaud <thibaud@jnuine.com>
2016-10-05 15:18:15 +0200

    style

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ import { NativeModules } from 'react-native';
 const { RNRandomBytes } = NativeModules;
 
 function toBuffer (nativeStr) {
-  return new Buffer(nativeStr, 'base64')
+  return new Buffer(nativeStr, 'base64');
 }
 
 export default function randomBytes (length) {
   return new Promise(function (resolve, reject) {
-    RNRandomBytes.randomBytes(length, function(err, base64String) {
+    RNRandomBytes.randomBytes(length, function (err, base64String) {
       if (err) {
         reject(err);
         return;
       }
 
       resolve(toBuffer(base64String));
-    })
-  })
+    });
+  });
 }
